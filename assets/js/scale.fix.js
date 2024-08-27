6dc5f487d4c6ab30b946e3ff882a03fd4c75e377
ChristianLentz <cjlsoccer23@gmail.com>
2024-08-27 14:11:49 -0400

    major updates to css and html

--- assets/js/scale.fix.js
+++ assets/js/scale.fix.js
@@ -1,26 +1,27 @@
-(function(document) {
+/**
+ * Scale webpage to make it viewable from PC or mobile
+ */
+
+(function (document) {
     var metas = document.getElementsByTagName('meta'),
-        changeViewportContent = function(content) {
+        changeViewportContent = function (content) {
             for (var i = 0; i < metas.length; i++) {
                 if (metas[i].name == "viewport") {
                     metas[i].content = content;
                 }
             }
         },
-        initialize = function() {
+        initialize = function () {
             changeViewportContent("width=device-width, minimum-scale=1.0, maximum-scale=1.0");
         },
-        gestureStart = function() {
+        gestureStart = function () {
             changeViewportContent("width=device-width, minimum-scale=0.25, maximum-scale=1.6");
         },
-        gestureEnd = function() {
+        gestureEnd = function () {
             initialize();
         };
-
-
     if (navigator.userAgent.match(/iPhone/i)) {
         initialize();
-
         document.addEventListener("touchstart", gestureStart, false);
         document.addEventListener("touchend", gestureEnd, false);
     }
